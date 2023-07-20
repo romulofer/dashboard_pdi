a6ddc34698e8c18ab71b3b925888115e9fb47d9b
Rômulo Fernandes Evangelista <romulo.fernandes@bridge.ufsc.br>
2023-07-20 15:28:23 -0300

    first commit

---
+++ backend/src/test/kotlin/com/romulo/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.romulo.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
