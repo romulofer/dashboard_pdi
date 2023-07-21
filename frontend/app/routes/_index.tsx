bccec623b5a5260e24f8e3a3cafd7099ea742fe0
Rômulo Fernandes Evangelista <romulo.fernandes@bridge.ufsc.br>
2023-07-21 17:51:22 -0300

    adicionado docker-compose

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,9 +1,15 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "Dashboard" }];
 };
 
 export default function Index() {
-  return <></>;
+  return (
+    <>
+      <h1>Boas-vindas ao Dashboard!</h1>
+      <Link to="login">Login</Link>
+    </>
+  );
 }
---
+++ frontend/app/routes/login.tsx
@@ -0,0 +1,3 @@
+export default function LoginPage() {
+  return <h1>Página de Login</h1>;
+}
