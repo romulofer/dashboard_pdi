5e727bb8142595615b69ad561a761f5f848bb30d
Rômulo Fernandes Evangelista <romulo.fernandes@bridge.ufsc.br>
2024-06-06 15:43:16 -0300

    página de login centralizada e estados para inputs estabelecidos

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,26 +1,47 @@
 import { css } from "@emotion/core";
 
 import { Button, Cell, Grid, TextInput } from "bold-ui";
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  const onUserChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUser(event.target.value);
+  };
+
+  const onPasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    console.log(`user: ${user}`);
+    console.log(`password: ${password}`);
+  };
   return (
     <Grid
       direction="column"
       alignItems="center"
       justifyContent="center"
       style={css`
-        height: 100%;
+        height: 100vh;
       `}
     >
       <Cell>
-        <TextInput placeholder="Username" />
+        <TextInput placeholder="Username" onChange={onUserChange} />
       </Cell>
       <Cell>
-        <TextInput placeholder="Password" type="password" />
+        <TextInput
+          placeholder="Password"
+          type="password"
+          onChange={onPasswordChange}
+        />
       </Cell>
       <Cell>
-        <Button kind="primary">Login</Button>
+        <Button kind="primary" onClick={handleButtonClick}>
+          Login
+        </Button>
       </Cell>
     </Grid>
   );
